783faa48f46c99f920838d90a98cc865bcb03eb4
zmj888 <running727@163.com>
2018-11-15 14:38:07 +0800

    a
    a

---
+++ src/Facades/Easykuaidi.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * This file is part of the cjl/easykuaidi.
+ *
+ * (c) cjl<running727@163.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Cjl\Easykuaidi\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Easykuaidi extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'easykuaidi';
+    }
+}
